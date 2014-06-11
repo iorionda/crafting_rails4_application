0a16c1fe11e533ff7a4ea603e19413e995938501
Iori ONDA <iori.onda@gmail.com>
2014-06-11 15:06:05 +0900

    Initial commit

---
+++ chapter-2/mail_form/lib/mail_form/base.rb
@@ -0,0 +1,27 @@
+module MailForm
+  class Base
+    include ActiveModel::AttributeMethods
+    attribute_method_affix :prefix => 'clear_', :suffix => '!'
+    attribute_method_prefix 'clear_'
+    attribute_method_suffix '?'
+
+    def self.attributes(*names)
+      attr_accessor(*names)
+      define_attribute_methods(names)
+    end
+
+    protected
+
+    def clear_attribute!(attributes)
+      send("#{attribute}=", nil)
+    end
+
+    def clear_attribute(attribute)
+      send("#{attribute}=", nil)
+    end
+
+    def attribute?(attribute)
+      send(attribute).present?
+    end
+  end
+end
